21a25112739b12438af6cd1ea9ce6cd12b8c4044
Stanislav <dubinin.stas@gmail.comgit config --global user.name Stanislav>
2023-09-12 19:40:52 +0400

    save

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,30 +1,61 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
 import styles from "./string.module.css";
 import { useState } from "react";
 import { Circle } from "../ui/circle/circle";
+import { ElementStates } from "../../types/element-states";
+import { resolve } from "path";
+
+
+type TInpArray = {
+  
+  state: ElementStates;
+  value: string;
+}
+
 
 export const StringComponent: React.FC = () => {
   const [input, setInput] = useState<string>();
   const [loader, setLoader] = useState<boolean>(false);
   const [isCircle, setIsCircle] = useState<boolean>(false);
+  const [circleArray, setCircleArray] = useState<TInpArray[] | null>(null);
+  const [dom, setDom] = useState<JSX.Element[]|null>()
+
+
+useEffect (()=>{
+ setCircleArray(null);
+ setIsCircle(false)
+ console.log(input)
+ input && !circleArray &&  createArrayCircle(input)
+}, [input])
+
+useEffect(()=>{
+ circleArray && renderCircle(circleArray)
+},[circleArray])
+
+
 
   const buttonClick = () => {
+if (!isCircle && input){
     setLoader(true);
     setIsCircle(true);
-    if (input) {
-      console.log(1);
-    }
+   
     setLoader(false);
+}
+ 
+
+
+   
   };
 
-  const reverse = (arr: string[]): string[] => {
-    const arrCircle = arr;
-    const length = arr.length;
+  const reverse = (arrCircle: TInpArray[]):TInpArray[] => {
+
+    const length = arrCircle.length;
     let start = 0;
     let end = length - 1;
+    
 
     while (start < end) {
       const temp = arrCircle[start];
@@ -33,32 +64,35 @@ export const StringComponent: React.FC = () => {
       start++;
       end--;
     }
-    return arr;
+    return arrCircle
   };
 
-  const visualizationAlgoritm = (string: string) => {
-    const length = string.length;
-    const arrString = Array.from(string);
-    let start = 0;
-    let end = length - 1;
+const createArrayCircle = (input:string)=>{
+ const inputArray = Array.from(input)
+ if (inputArray){
+const arrayCircle:TInpArray[] = inputArray.map((el)=>{
+   if (arrayCircle)  {
+   return [...arrayCircle,{state: ElementStates.Default, value: el }]
+   }
+   if (!arrayCircle) {return {state: ElementStates.Default, value: el }}
+   
+  })}}
 
-    const arrCircle = arrString.map((el) => {
-      return <Circle letter={el} />;
-    });
-    setTimeout(() => {
-      reverse(arrString);
-    }, 2000);
+const renderCircle = (arrInput:TInpArray[])=>{
+   const circleArray = arrInput.map((el) => {return <Circle state={el.state} letter={el.value} />})
+  setDom(circleArray);
+}
 
-    return arrCircle;
-  };
 
   return (
     <SolutionLayout extraClass={styles["string"]} title="Строка">
       <section className={styles["string__input-block"]}>
         <Input
           onChange={(e) => {
-            setIsCircle(false);
-            setInput(e.currentTarget.value);
+            console.log(e.currentTarget.value)
+        
+         
+           setInput(e.currentTarget.value);
           }}
           isLimitText={true}
           maxLength={11}
@@ -71,9 +105,9 @@ export const StringComponent: React.FC = () => {
           extraClass={styles["string__button"]}
         />
       </section>
-      {input && isCircle && (
+      {isCircle&& (
         <section className={styles["string__circle"]}>
-          {visualizationAlgoritm(input)}
+          {dom}
         </section>
       )}
     </SolutionLayout>
