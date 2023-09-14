32a58db950b929e5cd45879c71ae46d5b5f92d30
Stanislav <dubinin.stas@gmail.comgit config --global user.name Stanislav>
2023-09-14 18:51:48 +0400

    save

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -8,11 +8,16 @@ import { Direction } from "../../types/direction";
 import { randomArr } from "../../functions/func-random-arr";
 import { ElementStates } from "../../types/element-states";
 
+export type TRenderElement = {
+  val: number;
+  type: ElementStates;
+};
+
 export const SortingPage: React.FC = () => {
   const [radioCheck, setRadioCheck] = useState<"Bubble" | "Sample" | null>(
     null
   );
-  const [array, setArray] = useState<number[] | null>(null);
+  const [array, setArray] = useState<TRenderElement[] | null>(null);
   const [columns, setColumns] = useState<JSX.Element[] | null>(null);
 
   useEffect(() => {
@@ -21,27 +26,46 @@ export const SortingPage: React.FC = () => {
 
   const buttonClick = () => {
     setArray(randomArr());
+
     console.log(columns);
   };
 
-  const renderElements = (arr: number[]) => {
+  const buttonAscending = () => {
+    array && simplingAscending(array);
+  };
+
+  const renderElements = (arr: TRenderElement[]) => {
     if (arr) {
       const length = arr.length;
       let arrColumns: JSX.Element[] = [];
       arr.map((el) => {
-        arrColumns.push(<Column state={ElementStates.Default} index={el} />);
+        arrColumns.push(<Column state={el.type} index={el.val} />);
       });
       setColumns([...arrColumns]);
     }
   };
-
-  const simplingAscending = (arr: JSX.Element[]): JSX.Element[] => {
+  const simplingAscending = (arr: TRenderElement[]) => {
     const length = arr.length;
     let i = 0;
 
-    setInterval(() => {}, 1000);
+    const intervalI = setInterval(() => {
+      let y = i + 1;
+      arr[0].type = ElementStates.Changing;
+      const intervalY = setInterval(() => {
+        arr[y].type = ElementStates.Changing;
+        renderElements(arr);
+        if (y === length - 1) {
+          clearInterval(intervalY);
+        }
+        y++;
+      }, 500);
+      renderElements(arr);
+      if (i === length - 2) {
+        clearInterval(intervalI);
+      }
+      i++;
+    }, 1000);
   };
-
   return (
     <SolutionLayout title="Сортировка массива">
       <form className={styles["sorting__control-block"]}>
@@ -62,6 +86,7 @@ export const SortingPage: React.FC = () => {
           label={"Пузырёк"}
         />
         <Button
+          onClick={buttonAscending}
           text={"По возрастанию"}
           sorting={Direction.Ascending}
           extraClass={styles["sorting__button"]}
--- src/functions/func-random-arr.tsx
+++ src/functions/func-random-arr.tsx
@@ -1,8 +1,13 @@
-export const randomArr = (): number[] => {
+import { ElementStates } from "../types/element-states";
+import { TRenderElement } from "../components/sorting-page/sorting-page";
+export const randomArr = () => {
   const amountEl = Math.floor(Math.random() * (17 - 3)) + 3;
-  let arr: number[] = [];
+  let arr: TRenderElement[] = [];
   for (let i = 0; i < amountEl - 1; i++) {
-    arr.push(Math.floor(Math.random() * (100 - 0)) + 0);
+    arr.push({
+      val: Math.floor(Math.random() * (100 - 0)) + 0,
+      type: ElementStates.Default,
+    });
   }
 
   return arr;
